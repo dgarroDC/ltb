547c353363eb2e0bf9a13c91c8de7c3d4c81ab8b
Damián Garro <dgarro@dc.uba.ar>
2019-06-29 21:26:46 -0300

    Display all incoming updates

--- extensions/ltb_curl.cpp
+++ extensions/ltb_curl.cpp
@@ -1,30 +1,51 @@
 #include <iostream>
+#include <vector>
 #include <curl/curl.h>
 
 using namespace std;
 
-size_t ptb_curl_write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) {
+#ifndef ldpl_vector
+template<typename T>
+struct ldpl_list {
+    vector<T> inner_collection;
+};
+#endif
+
+string LTB_CURL_IN_URL;
+ldpl_list<string> LTB_CURL_IN_ARGNAMES;
+ldpl_list<string> LTB_CURL_IN_ARGVALUES;
+string LTB_CURL_OUT_RESPONSE;
+
+size_t ltb_curl_write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) {
     string& response = *(string *)userdata;
     size_t read_size = size * nmemb;
     response.append(ptr, read_size);
     return read_size;
 }
 
-string LTB_CURL_URL;
-string LTB_CURL_RESPONSE;
 void LTB_CURL_REQUEST(){
     CURL* curl = curl_easy_init();
     if (!curl) {
         cerr << "LTB curl error: could not start a libcurl easy session" << endl;
     }
-    curl_easy_setopt(curl, CURLOPT_URL, LTB_CURL_URL.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &LTB_CURL_RESPONSE);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, ptb_curl_write_callback);
-    LTB_CURL_RESPONSE = "";
+    // URL query string
+    string url = LTB_CURL_IN_URL;
+    string name, value;
+    for (int i = 0; i < LTB_CURL_IN_ARGNAMES.inner_collection.size(); i++) {
+        name = LTB_CURL_IN_ARGNAMES.inner_collection[i];
+        value = LTB_CURL_IN_ARGVALUES.inner_collection[i];
+        url += (i == 0 ? "?" : "&")
+             + string(curl_easy_escape(curl, name.c_str(), name.size())) + "="
+             + string(curl_easy_escape(curl, value.c_str(), value.size()));
+    }
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &LTB_CURL_OUT_RESPONSE);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, ltb_curl_write_callback);
+    LTB_CURL_OUT_RESPONSE = "";
     CURLcode res = curl_easy_perform(curl);
     if (res != CURLE_OK) {
         cerr << "LTB curl error: " << curl_easy_strerror(res) << endl;
-        cerr << "during request " << LTB_CURL_URL << endl;
+        cerr << "during request " << LTB_CURL_IN_URL << endl;
         exit(1);
     }
     curl_easy_cleanup(curl);
--- extensions/ltb_json.cpp
+++ extensions/ltb_json.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
+#include <vector>
 #include "../thirdparty/json.hpp"
 
 using namespace std;
 using json = nlohmann::json;
 
-string LTB_JSON_INPUT;
-string LTB_JSON_OUTPUT;
+#ifndef ldpl_vector
+template<typename T>
+struct ldpl_list {
+    vector<T> inner_collection;
+};
+#endif
+
+string LTB_JSON_IN_JSON;
+string LTB_JSON_IN_VALUE;
+string LTB_JSON_OUT_TEXT;
+ldpl_list<string> LTB_JSON_OUT_TEXTLIST;
 
 // Check if the request was successful from its response,
 // returning its 'result' field if OK, throwing error if not oK
 void LTB_JSON_GETRESULT() {
-    json j = json::parse(LTB_JSON_INPUT);
+    json j = json::parse(LTB_JSON_IN_JSON);
     bool ok = j["ok"];
     if (!ok) {
         cerr << "LTB TG error #" << j["error_code"] << ": " << j["description"] << endl;
         exit(1);
     }
-    LTB_JSON_OUTPUT = j["result"].dump();
+    LTB_JSON_OUT_TEXT = j["result"].dump();
+}
+
+// Parse a JSON array as a TEXT LIST
+void LTB_JSON_GETTEXTLIST() {
+    LTB_JSON_OUT_TEXTLIST.inner_collection.clear();
+    json j = json::parse(LTB_JSON_IN_JSON);
+    for (auto & element : j)
+        LTB_JSON_OUT_TEXTLIST.inner_collection.push_back(element.dump());
+}
+
+// Get a value from a JSON
+void LTB_JSON_GETVALUE() {
+    json j = json::parse(LTB_JSON_IN_JSON);
+    LTB_JSON_OUT_TEXT = j[LTB_JSON_IN_VALUE].dump();
 }
